9b583448af438c223edbfc67410ce201a810fb66
lazy-punk <patil.ojas.250211@outlook.com>
2024-09-09 17:36:14 +0000

    changed the settings.py file

--- backend/settings.py
+++ backend/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jackofalltrades.onrender.com', 'localhost']
 
 
 # Application definition
